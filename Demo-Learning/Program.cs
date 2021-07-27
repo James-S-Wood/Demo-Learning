9e44fc97c99a0d3d6618a5c70e23779a4df4caee
jameswood <James_S_Wood@outlook.com>
2021-07-27 04:35:08 -0400

    * .gitignore: Initial commit
    * Demo-Learning.sln:
    * Program.cs:
    * Demo-Learning.csproj:

---
+++ Demo-Learning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo_Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
